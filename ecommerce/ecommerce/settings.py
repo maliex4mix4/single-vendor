7b01944da00f20d431dbd17428a9679b47e1277a
Maliex <malikopeyemi5@gmail.com>
2019-12-18 22:02:01 +0100

    mysqlite database added

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'e-commerce'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'e-commerce.sqlite3'),
     }
 }
 
